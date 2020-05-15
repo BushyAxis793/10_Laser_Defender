3ba2b6ab69d3c9a1353d8db36abf20ad9d029dfd
Mikołaj Nawrocki <m.nawrocki793@gmail.com>
2020-05-15 12:32:02 +0200

    109. Introducing Particle Effects

---
+++ Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    [SerializeField] float backgroundScrollSpeed = .5f;
+    Material myMaterial;
+    Vector2 offset;
+
+    private void Start()
+    {
+        myMaterial = GetComponent<Renderer>().material;
+        offset = new Vector2(0, backgroundScrollSpeed);
+    }
+    private void Update()
+    {
+        myMaterial.mainTextureOffset += offset * Time.deltaTime;
+    }
+}
